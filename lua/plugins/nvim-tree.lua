803008ba41548804437743b34dad033a4181ce1c
IHA Yasushi <yasuc18@gmail.com>
2023-11-26 15:03:33 +0900

    feat: 🎸 add nvim-tree.lua & config
    nvim-tree.luaを使えるようにし、その設定を追加した。

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,46 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	requires = { "nvim-tree/nvim-web-devicons", "nvim-telescope/telescope.nvim" },
+	config = function()
+		-- netrwの無効化
+		vim.api.nvim_set_var("loaded_netrw", 1)
+		vim.api.nvim_set_var("loaded_netrwPlugin", 1)
+
+		require("nvim-tree").setup({
+			view = {
+				width = "20%",
+				signcolumn = "no",
+			},
+
+			renderer = {
+				highlight_git = true,
+				highlight_opened_files = "name",
+				icons = {
+					glyphs = {
+						git = {
+							unstaged = "!",
+							renamed = "»",
+							untracked = "?",
+							deleted = "✘",
+							staged = "✓",
+							unmerged = "",
+							ignored = "◌",
+						},
+					},
+				},
+			},
+
+			actions = {
+				expand_all = {
+					max_folder_discovery = 100,
+					exclude = { ".git", "target", "build" },
+				},
+			},
+			on_attach = require("config.nvim-tree-actions").on_attach,
+		})
+
+		vim.api.nvim_create_user_command("Ex", function()
+			vim.cmd.NvimTreeToggle()
+		end, {})
+	end,
+}
