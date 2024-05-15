d32f2ecd6a7a737ca7a007d17479d16d92bb1e56
IHA Yasushi <yasuc18@gmail.com>
2024-05-15 23:32:36 +0900

    MarkdownPreview用のブラウザ設定をOS毎に設定できるように変更

--- init.lua
+++ init.lua
@@ -1,9 +1,24 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd([[
+if vim.fn.has("mac") == 1 then
+	vim.cmd([[
+  function OpenMarkdownPreview (url)
+    execute "silent ! open -a Firefox -n --args --new-window " . a:url
+  endfunction
+  let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+]])
+elseif vim.fn.has("win64") == 1 then
+	vim.cmd([[
 function OpenMarkdownPreview (url)
   execute "silent ! chrome --new-window --app=" . a:url
 endfunction
 ]])
+else
+	vim.cmd([[
+function OpenMarkdownPreview (url)
+  execute "silent ! chrome --new-window --app=" . a:url
+endfunction
+]])
+end
 
 vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
